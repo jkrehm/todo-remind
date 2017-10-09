fac4153f892ec5ac1bd29732d0888cd77e749769
Jonathan Rehm <jonathan@rehm.me>
2017-10-08 23:31:36 -0700

    Rewrite application in Flask (Python)
    Dropbox dropped support for their v1 API and do not have an official SDK for their v2 API. I've also really been enjoying working in Python vs PHP, so it was a good excuse to migrate.
    
    Updates to the README will be forthcoming, as well as removal of the PHP application.

---
+++ db.py
@@ -0,0 +1,27 @@
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+
+class Dropbox(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    key = db.Column(db.String)
+    secret = db.Column(db.String)
+    access_token = db.Column(db.String)
+    file_location = db.Column(db.String)
+    cursor = db.Column(db.String)
+
+
+class Pushbullet(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    access_token = db.Column(db.String)
+
+
+class ToDo(db.Model):
+    __tablename__ = 'todo'
+
+    id = db.Column(db.Integer, primary_key=True)
+    text = db.Column(db.String)
+    date_time = db.Column(db.DateTime)
+
+
